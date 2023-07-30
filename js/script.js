c4a361b78e92195e1114a53fd43e776e2e4264e5
crouillebois fabien <FABB-49@hotmail.fr>
2023-07-31 01:29:20 +0200

    ancres

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
 
+function scrollToAncre(value){
+    document.getElementById(value).scrollIntoView();
+}
+
+
+/*
  window.onload = function(){
     document.querySelector('#input-color').addEventListener('change', function() {
         var r = document.querySelector(':root');
@@ -12,11 +18,11 @@ $('.navbar-nav>li>a').on('click', function(){
 
 
 };
+*/
 
-function scrollToAncre(value){
-    $('html, body').scrollTop($("#"+value).offset().top);
-}
 
+
+/*
 document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
       if (window.scrollY > 300) {
@@ -30,4 +36,4 @@ document.addEventListener("DOMContentLoaded", function(){
       } 
     });
 }); 
-
+*/
