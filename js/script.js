2a3d8139b828953f706ccb8f3a10b5524c85554b
crouillebois fabien <FABB-49@hotmail.fr>
2023-07-30 19:32:11 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+  });
+
+ window.onload = function(){
+    document.querySelector('#input-color').addEventListener('change', function() {
+        var r = document.querySelector(':root');
+        Array.from(document.querySelectorAll('.update-fill')).forEach(v => v.style.fill = this.value);
+        Array.from(document.querySelectorAll('.update-stroke')).forEach(v => v.style.stroke = this.value);
+        r.style.setProperty('--main-color', this.value);
+    });
+
+
+};
+
+document.addEventListener("DOMContentLoaded", function(){
+      
+    window.addEventListener('scroll', function() {
+         
+      if (window.scrollY > 360) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+         document.getElementById('navbar_top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+    });
+  }); 
+
