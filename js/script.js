e5cefc5c4daab3bd368a96bcaab048142f763207
crouillebois fabien <FABB-49@hotmail.fr>
2023-08-02 15:01:36 +0200

    ancres mobiles

--- js/script.js
+++ js/script.js
@@ -2,42 +2,6 @@ $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
 
-function scrollToAncre(value){
-    document.getElementById(value).scrollIntoView();
-}
-
 function callPhoneNumber(){
   window.open('tel:0673052057', '_self');
 }
-
-
-/*
- window.onload = function(){
-    document.querySelector('#input-color').addEventListener('change', function() {
-        var r = document.querySelector(':root');
-        Array.from(document.querySelectorAll('.update-fill')).forEach(v => v.style.fill = this.value);
-        Array.from(document.querySelectorAll('.update-stroke')).forEach(v => v.style.stroke = this.value);
-        r.style.setProperty('--main-color', this.value);
-    });
-
-
-};
-*/
-
-
-
-/*
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-      if (window.scrollY > 300) {
-        document.getElementById('navbar_top').classList.add('fixed-top');
-        navbar_height = document.querySelector('.navbar').offsetHeight;
-        document.body.style.paddingTop = navbar_height + 'px';
-      } else {
-         document.getElementById('navbar_top').classList.remove('fixed-top');
-         // remove padding top from body
-        document.body.style.paddingTop = '0';
-      } 
-    });
-}); 
-*/
